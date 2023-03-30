9c29806f5367bf567c130b32f65c1ce8de5b1f07
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-03-30 14:49:54 -0400

    Send raw html to client

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs/promises");
 
 const hostname = "127.0.0.1";
 const port = "3000";
 
+// async function readHtml() {
+//   const filename = "./index.html";
+//   try {
+//     const data = await fs.readFile(filename, "utf-8");
+//     console.log(data);
+
+//     // return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// readHtml();
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.write("hello");
-  res.end();
+  res.write("hello\n");
+
+  async function readHtml() {
+    const html = await fs.readFile("./index.html", "utf-8");
+    res.end(html);
+  }
+
+  readHtml();
+  //   res.end();
 });
 
 server.listen(port, hostname, () => {
