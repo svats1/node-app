db96aae1b67082a0465f89e9514e08e1f313d0be
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-03-31 16:02:08 -0400

    Finishing touches

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const http = require("http");
-const path = require("path");
-const fs = require("fs");
 const express = require("express");
-
 const app = express();
-
-const hostname = "127.0.0.1";
 const port = 8080;
 
 app.get("/", (req, res) => {
@@ -13,31 +7,20 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
+  console.log(req.url);
   res.sendFile("./about.html", { root: __dirname });
 });
 
 app.get("/contact-me", (req, res) => {
+  console.log(req.url);
   res.sendFile("./contact-me.html", { root: __dirname });
+});
+
+app.get("/404", (req, res) => {
+  console.log(req.url);
+  res.sendFile("./404.html", { root: __dirname });
 });
 
 app.listen(port, () => {
   console.log(`Now listening on ${port}`);
 });
-
-// const filename = "index.html";
-
-// fs.readFile(filename, (err, data) => {
-//   if (err) {
-//     throw err;
-//   }
-//   const server = http.createServer((req, res) => {
-//     res.writeHead(200, { "Content-Type": "text/html" });
-//     res.write(data);
-//     res.end();
-//     console.log(req.url.includes("about"));
-//   });
-
-//   server.listen(port, hostname, () => {
-//     console.log(`server listening at ${hostname}:${port}`);
-//   });
-// });
