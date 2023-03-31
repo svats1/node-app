ab8de747b4b0c55649f6df0ee263683838da16bf
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-03-31 15:18:25 -0400

    Use express and set up routes

--- index.js
+++ index.js
@@ -6,22 +6,38 @@ const express = require("express");
 const app = express();
 
 const hostname = "127.0.0.1";
-const port = "8080";
+const port = 8080;
 
-const filename = "index.html";
-
-fs.readFile(filename, (err, data) => {
-  if (err) {
-    throw err;
-  }
-  const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(data);
-    res.end();
-    console.log(req.url.includes("about"));
-  });
-
-  server.listen(port, hostname, () => {
-    console.log(`server listening at ${hostname}:${port}`);
-  });
+app.get("/", (req, res) => {
+  res.sendFile("./index.html", { root: __dirname });
 });
+
+app.get("/about", (req, res) => {
+  res.sendFile("./about.html", { root: __dirname });
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile("./contact-me.html", { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`Now listening on ${port}`);
+});
+
+// const filename = "index.html";
+
+// fs.readFile(filename, (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.write(data);
+//     res.end();
+//     console.log(req.url.includes("about"));
+//   });
+
+//   server.listen(port, hostname, () => {
+//     console.log(`server listening at ${hostname}:${port}`);
+//   });
+// });
